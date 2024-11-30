64824f728fb2f0b12fe89aad6d460d127c121405
sid <siddharthingle1010@gmail.com>
2024-11-30 12:30:07 +0530

    added framework structure

---
+++ src/main/java/PageRepository/HomePage.java
@@ -0,0 +1,32 @@
+package PageRepository;
+
+import org.openqa.selenium.WebDriver;
+
+import utility.AbstractComponent;
+//import utility.LogUtils;
+
+public class HomePage extends AbstractComponent  {
+	WebDriver driver;
+	int a=6;
+	int b=5;
+	
+	int newConlict=0;
+	
+	 
+	
+	
+	public HomePage(WebDriver driver) {
+        super(driver);
+		this.driver=driver;
+		//LogUtils.info("homepage login");
+		//
+		//
+	}
+
+	
+	
+
+	
+	
+
+}
